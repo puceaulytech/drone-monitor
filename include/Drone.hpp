dc2e0aea3c6167f8a1452d67d6766c97b5d46109
BlackJackGeary83 <robin.perdreau@gmail.com>
2022-12-01 19:22:27 +0100

    Update Drone.hpp

--- include/Drone.hpp
+++ include/Drone.hpp
@@ -9,14 +9,19 @@ class Drone : public QCustom3DItem {
   Q_OBJECT
 public:
   Drone(float resolution);
-  QVector3D getBezierPoint(QVector3D* points, int numPoints, float t);
+  // QVector3D getBezierPoint(QVector3D* points, int numPoints, float t);
   void computePath(int n);
-  // bezier
+
+  // telemetry
   QString m_meshFile;
   QVector3D m_position;
+  float yaw;
+  float picth;
+  float roll;
   QQuaternion m_rotation;
   QVector3D m_heading;
   float m_speed;
+  float height;
   QList<Waypoint> path;
   Waypoint* start;
   Waypoint* target;
@@ -24,8 +29,6 @@ public:
   float m_resolution;
 
 private:
-  // path
-  //  telemetry data
 public Q_SLOTS:
   void updateTelemetry();
 };
